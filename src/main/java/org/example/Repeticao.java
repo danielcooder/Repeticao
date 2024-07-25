9119056d5880c11bedbf0fe20cb401a3cec05ade
daniel cooder <danielcooder@gmail.com>
2024-07-25 17:03:42 -0300

    feat: repeticao com while

---
+++ src/main/java/org/example/Repeticao.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Repeticao {
+
+    public static void main(String[] args) {
+
+        int cc = 0;
+
+        do {
+            System.out.println("Cambalhota " + cc);
+
+            cc++;
+
+        } while (cc<11);
+
+    }
+
+}
